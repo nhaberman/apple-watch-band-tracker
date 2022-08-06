d57e7086c1f78fb0d9de03f5beaef840746d17b0
Nick Haberman <65823359+nhaberman@users.noreply.github.com>
2022-08-05 22:18:24 -0500

    add initial files

---
+++ Apple Watch Band Tracker/Shared/Apple_Watch_Band_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Watch_Band_TrackerApp.swift
+//  Shared
+//
+//  Created by Nick Haberman on 8/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Watch_Band_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
