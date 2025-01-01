3aec7b62e6146f0fd583f6174a348e389a25e5fc
Nick Haberman <65823359+nhaberman@users.noreply.github.com>
2025-01-01 15:57:06 -0600

    first addition of widget files

---
+++ Apple Watch Band Tracker/AppleWatchBandTrackerWidget/AppleWatchBandTrackerWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  AppleWatchBandTrackerWidgetBundle.swift
+//  AppleWatchBandTrackerWidget
+//
+//  Created by Nick Haberman on 1/1/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct AppleWatchBandTrackerWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        AppleWatchBandTrackerWidget()
+        AppleWatchBandTrackerWidgetControl()
+    }
+}
